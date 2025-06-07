82106a183d39235ab407c917fffa804e0ae0353a
mohaaz12 <mohaaz3004@gmail.com>
2025-06-08 00:10:26 +0300

    arthimetic

---
+++ index.js
@@ -0,0 +1,37 @@
+// Basic math functions
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Increment and decrement
+
+function increment(n) {
+  return n + 1;
+}
+
+function decrement(n) {
+  return n - 1;
+}
+
+// Parsing numbers
+
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
